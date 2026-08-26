7dc11a6226fb975bd295d023e9c1f82133db00ea
jo1127-here <jo20041127@gmail.com>
2026-08-26 20:45:00 +0800

    Fix console log for students array

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ let students = 40;
 
 console.log(course);
 console.log(students);
-
 let data = [10, 20, 30, 40, 50];
 
 console.log(data);
-
 let student = {
     name: "Alice",
     score: 85
@@ -15,7 +13,6 @@ let student = {
 
 console.log(student.name);
 console.log(student.score);
-
 let students = [
     {name: "Alice", score: 85},
     {name: "Bob", score: 72},
